78e494b82ffd2b5c7828f48d33e0cb126af66852
Arunmozhi <arunmozhi@opencraft.com>
2022-12-20 19:03:44 +0530

    feat: adds the AddAJAXButton Block
    This commit introduces the following changes:
    * Rename the AddExtraBlock pipeline step to AddDebugInfoBlock
    * Update the AddDebugInfoBlock's run_filter to accept the block as the
      first argument
    * Adds the AddAJAXButtonBlock pipeline step which allows calling a JSON
      Handler names "dummy_json" of the XBlock passed to it. This step
      demonstrates the ability to call the XBlock handlers from code
      injected by the external applcations using this pipeline.

--- openedx_filter_demos/pipeline.py
+++ openedx_filter_demos/pipeline.py
@@ -9,9 +9,9 @@ from openedx_filters import PipelineStep
 logger = logging.getLogger(__name__)
 
 
-class AddExtraBlock(PipelineStep):
+class AddDebugInfoBlock(PipelineStep):
     """
-    Adds an block at the end of a VerticalBlock.
+    Adds extra HTML to the fragment.
 
     Example Usage:
 
@@ -21,18 +21,20 @@ class AddExtraBlock(PipelineStep):
             "org.openedx.learning.vertical_block.render.completed.v1": {
                 "fail_sliently": false,
                 "pipeline": [
-                    "openedx_filter_demos.pipeline.AddExtraBlock"
+                    "openedx_filter_demos.pipeline.AddDebugInfo"
                 ]
             }
         }
     """
 
-    def run_filter(self, fragment, context, view):
+    def run_filter(self, block, fragment, context, view):
         """Pipeline Step implementing the Filter"""
 
         debug_div = f"""<div class="debug-block" style="border: 2px solid red; padding: 1rem; margin: 1rem;">
+<h5>Debug Info</h5>
 <pre>
-fragment = {fragment}
+block = {str(block)}
+fragment = {str(fragment)}
 context = {json.dumps(context, indent=2)}
 view = {view}
 </pre>
@@ -40,10 +42,74 @@ view = {view}
 """
         fragment.content = f"""<div class="openedx-filter-demos">{fragment.content}{debug_div}</div>"""
 
-        logger.info("=========== AddExtraBlock Pipeline Step Start ==========")
+        logger.info("=========== AddDebugInfoBlock Pipeline Step Start ==========")
         logger.info("Fragment: %s", str(fragment))
         logger.info("Context: %s", context)
         logger.info("View: %s", view)
-        logger.info("=========== AddExtraBlock Pipeline Step Stop ==========")
+        logger.info("=========== AddDebugInfoBlock Pipeline Step End ==========")
 
-        return {"fragment": fragment, "context": context, "view": view}
+        return {"block": block, "fragment": fragment, "context": context, "view": view}
+
+
+class AddAJAXButtonBlock(PipelineStep):
+    """
+    Injects an button and JS for making a AJAX POST request to the "dummy_json" JSON Handler of the XBlock.
+
+    So, add a JSON like the one below to your XBlock before adding this step to your filter pipeline.
+
+    .. code-block::
+
+        @XBlock.json_handler
+        def dummy_json(self, data, suffix=""):
+            print("[DUMMY JSON Handler]")
+            print(f"Data passed to the endpoint: {data}")
+            return {"status": "success"}
+
+    Example Configuration:
+
+    .. code-block::
+
+        "OPENEDX_FILTERS_CONFIG": {
+            "org.openedx.learning.vertical_block.render.completed.v1": {
+                "fail_silently": false,
+                "pipeline": [
+                    "openedx_filter_demos.pipeline.AddAJAXButton"
+                ]
+            }
+        }
+    """
+    def run_filter(self, block, fragment, context, view):
+        """
+        Pipeline step implementing the filter.
+        """
+        url = block.runtime.handler_url(block, "dummy_json")
+        script = """
+<script>
+function callDummyJsonHandler(url) {
+    var csrf_token = document.cookie.split(";").find(c => c.startsWith("csrftoken="))?.split("=")[1];
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify(["some", "data"]), 
+        headers: {
+            "Content-Type": "application/json",
+            "X-CSRFToken": csrf_token,
+        }
+    })
+    .then(res => res.json())
+    .then(data => document.querySelector("#response").innerHTML = JSON.stringify(data));
+}
+</script>
+"""
+        snippet = f"""<div class="debug-block" style="border: 2px solid red; padding 1rem; margin: 1rem;">
+<h5>Ajax Demo</h5>
+<p><strong>NOTE!</strong> This requires a "dummy_json" JSON Handler to be defined in the VerticalBlock to test.</p>
+<button onclick="callDummyJsonHandler('{str(url)}')">Send Data</button>
+
+<h5 style="maring-top: 1rem;">Response</h5>
+<pre id="response"></pre>
+{script}
+</div>
+"""
+        fragment.content = f"""<div class="openedx-filter-demos">{fragment.content}{snippet}</div>"""
+
+        return dict(block=block, fragment=fragment, context=context, view=view)
